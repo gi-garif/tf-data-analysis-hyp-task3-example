443bd8db227a56e0727fc59efe55a08e03e2a7ac
gi-garif <84095383+gi-garif@users.noreply.github.com>
2023-04-13 09:42:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ttest_1samp
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 33217853 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
