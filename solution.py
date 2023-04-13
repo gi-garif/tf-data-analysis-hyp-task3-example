dc1f3d1df7adba72e685fbe87ad2725dfd4ce808
gi-garif <84095383+gi-garif@users.noreply.github.com>
2023-04-13 11:10:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,20 +4,16 @@ from scipy.stats import ttest_1samp
 
 chat_id = 33217853 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-
-    data = pd.read_csv("/hyp3_historical_data.csv")
-    
-    d = data.iloc[0].to_list()
 
     threshold = 300
 
     alpha = 0.1
 
-    t_statistic, p_value = ttest_1samp(data, threshold)
+    t_statistic, p_value = ttest_1samp(x, threshold)
 
     if p_value < alpha:
         result = True  
