05971234e85ffefc563a24d53db42c51111ff7b8
gi-garif <84095383+gi-garif@users.noreply.github.com>
2023-04-13 09:36:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import ttest_1samp
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,20 @@ def solution(...) -> bool: # Одна или две выборке на вход
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    data = pd.read_csv("/hyp3_historical_data.csv")
+    
+    d = data.iloc[0].to_list()
+
+    threshold = 300
+
+    alpha = 0.1
+
+    t_statistic, p_value = ttest_1samp(data, threshold)
+
+    if p_value < alpha:
+        result = True  
+    else:
+        result = False  
+
+    return result # Ваш ответ, True или False
